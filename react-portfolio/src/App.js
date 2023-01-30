8e125aecbb0ee542568f0d6a75f72cdcdff527ed
nelaturiankitha <nelaturiankitha@gmail.com>
2023-01-30 22:06:20 +0530

    Added sidebar

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import './App.scss';
+import { Routes,Route } from 'react-router-dom';
+import Layout from './Components/Layout';
+
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element = {<Layout />} />
+    </Routes>
+    </>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
