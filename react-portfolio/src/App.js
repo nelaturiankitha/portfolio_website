56ab185a326a8f85190bad1c569bf48be773fb87
nelaturiankitha <nelaturiankitha@gmail.com>
2023-01-31 14:32:14 +0530

    Edite home page - Written intro

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,15 +2,18 @@
 import './App.scss';
 import { Routes,Route } from 'react-router-dom';
 import Layout from './Components/Layout';
+import Home from './Components/Home';
 
 
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element = {<Layout />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ react-portfolio/src/Components/Home/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1> Hi, <br/> I'm Ankitha
+                <br/>
+                 backend developer.</h1>
+                 <h2>I know how to fix that bug on line 255!</h2>
+            </div>
+        </div>
+      )
+}
+
+export default Home
